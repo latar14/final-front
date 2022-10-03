249ee815a004b58c0d8e538ff7c914da98b09b8c
Ashwash <latar14@bk.ru>
2022-10-03 17:18:33 +0300

    Сделал хидер

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Header from './components/Header/Header';
+
+const App: React.FunctionComponent = () => {
+  
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+};
+
+export default App;
